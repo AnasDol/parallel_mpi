d65b06b585a083a8cc7392f3a1c882b30f2288d0
AnasDol <anastasiadol2002@gmail.com>
2023-10-24 19:14:24 +0700

    logs are wrritten to file

--- src/task.c
+++ src/task.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define T 100 // постоянная температура в правом верхнем углу
@@ -12,9 +13,23 @@
 
 void initialize(double** temperature, int m, int n);
 double get_new_temp(double** temperature, int rows, int cols, int current_row, int current_col, double R);
-void compute_temperature(double** temperature, int m, int n, double R);
+int compute_temperature(double** temperature, int m, int n, double R, FILE* logfile);
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        printf("Enter log filename as command prompt argument\n");
+        return 0;
+    }
+
+    char filename[255];
+    strncpy(filename, argv[1], 255);
+
+    FILE* logfile = fopen(filename, "w+");
+    if (!logfile) {
+        printf("File not found\n");
+        return 0;
+    }
 
     printf("Row number (>=2): ");
     int m; // размер области по вертикали
@@ -44,13 +59,14 @@ int main() {
 
     initialize(temperature, m, n);
 
-    compute_temperature(temperature, m, n, R);
+    int count = compute_temperature(temperature, m, n, R, logfile);
+    printf("Count: %d\n", count);
 
-    if (m <= 10 && n <= 10) {
-        for (int i = 0; i < n; i++) {
-            printf("%.2lf\t", temperature[0][i]);
-        }
+    for (int i = 0; i < n; i++) {
+        printf("%.2lf\t", temperature[0][i]);
     }
+
+    printf("\nIntermediate results is saved in the log file.\n", count);
 
     // Вывод распределения температур
     // for (int i = 0; i < m; i++) {
@@ -66,6 +82,8 @@ int main() {
         free(temperature[i]);
     }
     free(temperature);
+
+    fclose(logfile);
     
     return 0;
 }
@@ -89,6 +107,7 @@ void initialize(double** temperature, int rows, int cols) {
 double get_new_temp(double** temperature, int rows, int cols, int current_row, int current_col, double R) {
 
     if (current_col == 0 || current_col == cols-1 || current_row == rows-1) return temperature[current_row][current_col]; // постоянная температура
+    else if (pow(current_row-round((double)(rows/2-1)), 2) + pow(current_col-round((double)(cols/2)), 2) <= R*R) return 0.0;
 
     double newTemp = 0.0;
     int count = 0;
@@ -109,7 +128,7 @@ double get_new_temp(double** temperature, int rows, int cols, int current_row, i
 
 }
 
-void compute_temperature(double** temperature, int rows, int cols, double R) {
+int compute_temperature(double** temperature, int rows, int cols, double R, FILE* logfile) {
 
     // Метод последовательных приближений
     double diff = 1.0;  // Разница между текущей и предыдущей итерациями
@@ -120,7 +139,18 @@ void compute_temperature(double** temperature, int rows, int cols, double R) {
         new_temp[i] = (double*)malloc(cols * sizeof(double));
     }
 
+    if (logfile != NULL) {
+            fprintf(logfile, "[%d] ", iter);
+            for (int i = 0; i < cols; i++) {
+                fprintf(logfile, "%.2lf ", temperature[0][i]);
+            }
+            fprintf(logfile, "\n");
+        }
+
     while (diff > EPSILON) {
+
+        iter++;
+
         diff = 0.0;
 
         for (int i = 0; i < rows; i++) {
@@ -138,15 +168,21 @@ void compute_temperature(double** temperature, int rows, int cols, double R) {
             }
         }
 
-        iter++;
-    }
+        if (logfile != NULL) {
+            fprintf(logfile, "[%d] ", iter);
+            for (int i = 0; i < cols; i++) {
+                fprintf(logfile, "%.2lf ", temperature[0][i]);
+            }
+            fprintf(logfile, "\n");
+        }
 
-    printf("Count: %d", iter);
+    }
 
     for (int i = 0;i<rows;i++) {
         free(new_temp[i]);
     }
     free(new_temp);
 
+    return iter;
     
 }
